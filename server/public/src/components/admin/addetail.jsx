543f2bebb38af964c7f919a0496e4de59f7ccf89
Tom Cardoso <tcardoso@globeandmail.com>
2019-07-25 10:58:43 -0400

    Removes code that is hiding Sponsored text

--- server/public/src/components/admin/addetail.jsx
+++ server/public/src/components/admin/addetail.jsx
@@ -13,14 +13,6 @@ export class AdDetailUnconnected extends React.Component {
       this.props.getOneAd(ad_id);
     }
   }
-  componentDidUpdate() {
-    Array.from(document.querySelectorAll(".clearfix._42ef ._5u5j span span"))
-      .concat(
-        Array.from(document.querySelectorAll(".clearfix._42ef ._5u5j span div"))
-      )
-      .filter(t => t.textContent.length == 1)
-      .forEach(t => t.remove());
-  }
 
   render() {
     if (this.props.ad) {
